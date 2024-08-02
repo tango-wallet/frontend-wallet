cbe114dc2b5a9b93d7c31f563d1be2bd3b8e1ab5
FxIvan <almendraivan210814@gmail.com>
2024-08-02 20:41:18 -0300

    fix build ?

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-
+import { signIn } from "next-auth/react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -8,6 +9,15 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
         </p>
+        <button
+          onClick={async () => {
+            await signIn("google", {
+              callbackUrl: "/protect/portfolio",
+            });
+          }}
+        >
+          Ingresa con google
+        </button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
