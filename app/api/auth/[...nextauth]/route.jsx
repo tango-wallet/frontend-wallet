450651b54e4c2de45f1d17cd93347dcc8f6ce043
FxIvan <almendraivan210814@gmail.com>
2024-08-02 20:02:22 -0300

    first commit

---
+++ app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,129 @@
+import {
+  NEXT_PUBLIC_BACKEND,
+  NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+  NEXTAUTH_SECRET,
+} from "constants/env";
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/login",
+    error: "/error",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Tango Wallet",
+      credentials: {
+        loginEmail: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        loginPassword: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { loginEmail, loginPassword } = credentials;
+        const response = await fetch(`${NEXT_PUBLIC_BACKEND}/user/login`, {
+          method: "POST",
+          body: JSON.stringify({
+            email: loginEmail,
+            password: loginPassword,
+          }),
+          headers: {
+            cache: "no-store",
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await response.json();
+
+        if (!data || !data.user || data.statusCode >= 400) {
+          throw new Error(btoa(data.message) || "Error");
+        }
+
+        return {
+          id: data.user._id,
+          provider: "credentials",
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile, trigger, user, session }) {
+      if (user && (!account || account.provider != "google")) {
+        token.provider = "credentials"; //Provider
+        token.accessToken = user[0].id_token; //Token generado por nuestro Backend
+        token.user = user[0]; //Datos del usuario
+        token.accessToken = user[0].token;
+      } else if (account && account.provider === "google") {
+        token.provider = "google"; //Provider
+        token.accessToken = account.accessToken; //Token generado por nuestro Backend
+        token.user = account; //Datos del usuario
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.provider = token.provider; //Provider
+      session.accessToken = token.accessToken; //Token generado por nuestro Backend
+      session.user = token.user; //Datos del usuario
+      return session; //con useSession() en el cliente("use client") podemos acceder a estos datos
+    },
+    async signIn({ account, profile }) {
+      try {
+        if (account.provider === "google") {
+          console.log("------------------------------------------");
+          console.log("ID Token de google: ", account.id_token);
+          console.log("------------------------------------------");
+          const response = await fetch(
+            `${NEXT_PUBLIC_BACKEND}/users/login/google`,
+            {
+              method: "POST",
+              body: JSON.stringify({
+                code: account.id_token,
+              }),
+              headers: {
+                cache: "no-store",
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const data = await response.json();
+          console.log("Data --->", data);
+          if (!data) {
+            console.log("Error Data --->");
+            throw new Error(window.btoa(data.message) || "Error");
+          }
+
+          if (data.user._id) {
+            account.id = data.user._id;
+          }
+          account.accessToken = data.token;
+          return {
+            id: data.user._id,
+            provider: "google",
+          };
+        }
+        return true;
+      } catch (error) {
+        console.log("Error en el callback signIn", error);
+        throw new Error(btoa(error) || "Error");
+      }
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ middleware.js
@@ -0,0 +1,43 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return NextResponse.next();
+  },
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null,
+    },
+    pages: {
+      signIn: "/auth/login",
+    },
+  }
+);
+
+export default function middleware(req) {
+  try {
+    const privatePathnameRegex = RegExp(`^/protect/.*$`, "i");
+
+    // Check if the current route is a private page
+    const isPrivatePage = privatePathnameRegex.test(req.nextUrl.pathname);
+
+    if (isPrivatePage) {
+      console.log("Private page detected, running authMiddleware");
+      return authMiddleware(req);
+    } else {
+      console.log("Public page detected, continuing");
+      return NextResponse.next();
+    }
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
