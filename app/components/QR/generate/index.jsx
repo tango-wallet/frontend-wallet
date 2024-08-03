ec7c8e9f8d151be23f036dde26bc8f66912887a0
FxIvan <almendraivan210814@gmail.com>
2024-08-03 07:59:56 -0300

    generar codigo qr

--- app/components/QR/generate/index.jsx
+++ app/components/QR/generate/index.jsx
@@ -1,3 +1,30 @@
-export default function GenerateQR() {
-  return <div></div>;
+import Link from "next/link";
+import QRCode from "react-qr-code";
+
+export default function GenerateQR({ value }) {
+  return (
+    <div className="w-80">
+      <QRCode
+        size={80}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={value}
+        viewBox={`0 0 80 80`}
+      />
+
+      <div>
+        <p className="text-center text-gray-400 text-sm mt-2">
+          Escanea este código QR para realizar el pago
+        </p>
+      </div>
+
+      <div className="w-full text-center mt-10">
+        <Link
+          href="/protect/dashboard"
+          className="text-center text-blue-400 text-sm mt-2"
+        >
+          Regresar
+        </Link>
+      </div>
+    </div>
+  );
 }
--- screen/deposit/qr/index.jsx
+++ screen/deposit/qr/index.jsx
@@ -4,59 +4,78 @@ import WarningIcon from "public/WarningIcon.png";
 import Image from "next/image";
 import IconoScrollRounded from "public/IconoScrollRounded.png";
 import useForm from "../../../hooks/useForm";
+import GenerateQR from "app/components/QR/generate";
 
 export default function ScreenQRDeposit() {
-  const [optionSelected, setOptionSelected] = useState(null);
+  const [urlWithParams, setUrlWithParams] = useState(null);
   const [form, handleChange, resetForm] = useForm({
     amount: "",
+    myWallet: "0x1234567890",
   });
 
+  const handleNextStep = (e) => {
+    const hashedParams = `http://localhost:3000/protect/deposit/qr?amount=${form.amount}&myWallet=${form.myWallet}`;
+    setUrlWithParams(hashedParams);
+  };
+
   return (
-    <div className="h-[80vh] min-h-[600px] flex flex-col items-center mx-4 justify-between my-4">
-      <div>
-        <div className="flex w-full justify-center my-8">
-          <Image src={IconoScrollRounded} alt="Icono Scroll" />
-        </div>
-        <div className="">
+    <div
+      className={`h-[80vh] min-h-[600px] flex flex-col items-center mx-4 ${
+        !urlWithParams ? "justify-between" : "justify-center"
+      } my-4`}
+    >
+      {!urlWithParams && (
+        <>
           <div>
-            <form className="w-full text-center">
-              <label className="text-xl text-gray-400 font-semibold">
-                Puedes ingresar un monto
-              </label>
-              <input
-                type="number"
-                name="amount"
-                onChange={handleChange}
-                value={form.amount}
-                placeholder="0.00"
-                className="py-6 text-center text-7xl w-full rounded-lg mt-8 text-black font-bold"
-              />
-            </form>
+            <div className="flex w-full justify-center my-8">
+              <Image src={IconoScrollRounded} alt="Icono Scroll" />
+            </div>
+            <div className="">
+              <div>
+                <form className="w-full text-center">
+                  <label className="text-xl text-gray-400 font-semibold">
+                    Puedes ingresar un monto
+                  </label>
+                  <input
+                    type="number"
+                    name="amount"
+                    onChange={handleChange}
+                    value={form.amount}
+                    placeholder="0.00"
+                    className="py-6 text-center text-7xl w-full rounded-lg mt-8 text-black font-bold"
+                  />
+                </form>
+              </div>
+            </div>
           </div>
+          <div className=" bg-orange-100 rounded-lg px-2 py-2 border border-orange-500">
+            <div className="flex flex-row items-center  pb-4">
+              <Image src={WarningIcon} alt="Warning Icon" className="me-4" />
+              <h3 className="text-xl text-orange-400">¡Atención!</h3>
+            </div>
+            <div>
+              <p className="text-black text-xs">
+                Solo puedes depositar EARS en la red de Scroll. Asegúrate de
+                seleccionar la red correcta antes de realizar tu depósito para
+                evitar pérdidas de fondos.
+              </p>
+            </div>
+          </div>
+          <div className="w-full">
+            <button
+              className={`${"bg-[#222fe6]"} text-white font-bold py-4 px-6 items-center rounded-lg w-full text-center`}
+              onClick={(e) => handleNextStep(e)}
+            >
+              Continuar
+            </button>
+          </div>
+        </>
+      )}
+      {urlWithParams && (
+        <div className="w-full mt-8 flex justify-center">
+          <GenerateQR value={urlWithParams} />
         </div>
-      </div>
-      <div className=" bg-orange-100 rounded-lg px-2 py-2 border border-orange-500">
-        <div className="flex flex-row items-center  pb-4">
-          <Image src={WarningIcon} alt="Warning Icon" className="me-4" />
-          <h3 className="text-xl text-orange-400">¡Atención!</h3>
-        </div>
-        <div>
-          <p className="text-black text-xs">
-            Solo puedes depositar EARS en la red de Scroll. Asegúrate de
-            seleccionar la red correcta antes de realizar tu depósito para
-            evitar pérdidas de fondos.
-          </p>
-        </div>
-      </div>
-      <div className="w-full">
-        <button
-          className={`${
-            optionSelected != null ? "bg-[#222fe6]" : "bg-slate-600"
-          } text-white font-bold py-4 px-6 items-center rounded-lg w-full text-center`}
-        >
-          Continuar
-        </button>
-      </div>
+      )}
     </div>
   );
 }
