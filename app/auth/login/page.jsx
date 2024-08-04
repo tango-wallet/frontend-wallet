b8b995bdf6f0aba248d530c60ae653aa48c2a597
FxIvan <almendraivan210814@gmail.com>
2024-08-04 08:20:21 -0300

    todoa actualizado para david

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,6 +1,16 @@
 import ScreenLogin from "screen/auth/login";
 
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
 export default async function MainSSRLogin() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/protect/dashboard");
+  }
+
   return (
     <main>
       <ScreenLogin />
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,7 +1,16 @@
 import ScreenRegister from "screen/auth/register";
 import { signIn } from "next-auth/react";
 
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
 export default async function MainSSRRegister() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/protect/dashboard");
+  }
   return (
     <main className="">
       <ScreenRegister />
